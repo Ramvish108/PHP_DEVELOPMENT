7fcc62fdf7368f035d85422ca4087addf7643061
Ram Vishwakarma <rv7029919@gmail.com>
2025-05-02 22:25:21 +0530

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First_Program</title>
+    <style>
+    h1{
+        color:red;
+        text-align: center;
+        font-family: Arial, Helvetica, sans-serif
+    }
+    body{
+        background-color: grey;;
+        margin: 20px;
+    }
+    .php-output {
+            color: green;
+            font-size: 18px;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            background-color: #e0e0e0;
+            padding: 10px;
+            border-radius: 5px;
+    }
+  
+</style>
+</head>
+
+<body>
+    <h1>Printing "hello world "from php</h1>
+</body>
+</html>
+
+
+
+
+<p class="php-output">
+<?php
+echo"hello world ";
+?></p>
