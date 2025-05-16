2fc57ae251e97d817d738e8a0af7289f985dfe12
Ram Vishwakarma <rv7029919@gmail.com>
2025-05-16 17:50:46 +0530

    Add files via upload

---
+++ String.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Types</title>
+    <style>
+    h1{
+        color:red;
+        text-align: center;
+        font-family: Arial, Helvetica, sans-serif
+    }
+    body{
+        background-color: grey;;
+        margin: 20px;
+    }
+    .php-output {
+            color: green;
+            font-size: 18px;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            background-color: #e0e0e0;
+            padding: 10px;
+            border-radius: 5px;
+    }
+  
+</style>
+</head>
+
+<body>
+    <h1>PHP string </h1>
+    <h2>
+        value of a ="hello pappu"
+    </h2>
+</body>
+</html>
+
+
+
+
+<p class="php-output">
+<?php
+$a="hello pappu";
+
+var_dump($a);
+echo"value of a is =$a";
+?></p>
